1d7f16dbc738bea9a038e9eb51aced2fd27f5c71
Nikita Ermolenko <otbivnoe@Nikitas-Mac-mini.local>
2017-01-08 22:26:08 +0600

    Add ElegantSwift file in order to add github `swift` label :)

---
+++ ElegantSwift.swift
@@ -0,0 +1,11 @@
+//
+//  ElegantSwift.swift
+//  ElegantSwift
+//
+
+print("ELEGANT SWIFT!!!")
+print("ELEGANT SWIFT!!!")
+print("ELEGANT SWIFT!!!")
+print("ELEGANT SWIFT!!!")
+print("ELEGANT SWIFT!!!")
+
